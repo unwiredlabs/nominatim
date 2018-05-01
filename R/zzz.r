3636aca841028b2f89f9794866b793773a6f24fb
Gopi <gopi-ar@users.noreply.github.com>
2018-05-01 14:46:13 +0530

    Update zzz.r (#1)
    Added LocationIQ urls

--- R/zzz.r
+++ R/zzz.r
@@ -5,19 +5,14 @@
 # DELAY <- 0.5
 
 # URL base for various Nominatim operations
-# search_base <- "http://open.mapquestapi.com/nominatim/v1/search.php"
 lookup_base <- "http://nominatim.openstreetmap.org/lookup"
-reverse_base <- "http://open.mapquestapi.com/nominatim/v1/reverse.php"
-
-# search_base <- "http://nominatim.openstreetmap.org/search"
-# lookup_base <- "http://nominatim.openstreetmap.org/lookup"
-# reverse_base <- "http://nominatim.openstreetmap.org/reverse"
+reverse_base <- "http://us1.locationiq.org/v1/reverse.php"
+search_base <- "https://us1.locationiq.org/v1/search.php"
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Data (c) OpenStreetMap contributors, ODbL 1.0. http://www.openstreetmap.org/copyright")
-  packageStartupMessage("Nominatim Usage Policy: http://wiki.openstreetmap.org/wiki/Nominatim_usage_policy")
-  packageStartupMessage("MapQuest Nominatim Terms of Use: http://info.mapquest.com/terms-of-use/")
+  packageStartupMessage("API Service (c) LocationIQ, Data (c) OpenStreetMap contributors, ODbL 1.0. http://www.openstreetmap.org/copyright")
+  packageStartupMessage("Sign-up to get a free Developer token: https://locationiq.org")
   options(NOMINATIM.TIMEOUT=5)
   options(NOMINATIM.DELAY=0.5)
-  options(NOMINATIM.search_base="http://open.mapquestapi.com/nominatim/v1/search.php")
+  options(NOMINATIM.search_base="https://us1.locationiq.org/v1/search.php")
 }
